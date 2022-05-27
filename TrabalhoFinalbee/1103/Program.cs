07d8cbf66f955563c0aa2c9c27f85d7b6a9ae111
Guest 03 <guest03@alunos.sc.senac.br>
2022-05-26 22:09:50 -0300

    envio trabalho final na MAIN

--- TrabalhoFinalbee/1103/Program.cs
+++ TrabalhoFinalbee/1103/Program.cs
@@ -6,16 +6,37 @@ namespace _1103
     {
         static void Main(string[] args)
         {
+            int h1, m1, h2, m2, tempo;
 
-            var entrada = 0;
-            int[] numeros;
+            while (true) 
+            {
 
-            int[] tempo = new int[4] {0, 0, 0, 0};
-            int
+              String[] linha = Console.ReadLine().Split(' ');
 
-            
-            
-            
+                h1 = int.Parse(linha[0]);
+                m1 = int.Parse(linha[1]);
+                h2 = int.Parse(linha[2]);
+                m2 = int.Parse(linha[3]);
+
+                if (h1 == 0 && h2 == 0 && m1 == 0 && m2 == 0)
+                    break;
+
+                if((h1 > h2) || (h1 == h2) && m1 > m2)
+                h2 += 24;
+
+                h1 *= 60;
+                h2 *= 60;
+
+                int hora1, hora2;
+                hora1 = h1 + m1;
+                hora2 = h2 + m2;
+
+                tempo = hora2 - hora1;
+
+                Console.WriteLine($"{tempo}");
+
+
+            }
 
 
         }
--- TrabalhoFinalbee/1140/Program.cs
+++ TrabalhoFinalbee/1140/Program.cs
@@ -5,8 +5,38 @@ namespace _1140
     internal class Program
     {
         static void Main(string[] args)
-        {
-            
+        { 
+
+
+            string entrada = "";
+
+            while (true)
+            {
+                entrada = Console.ReadLine();
+                if (entrada == "*")
+                    break;
+
+                if()
+
+
+            }
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         }
     }
 }
