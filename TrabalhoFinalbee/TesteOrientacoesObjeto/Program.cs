7285b57f40c66a579d81b5dbb64edb4257f81df5
Guest 03 <guest03@alunos.sc.senac.br>
2022-06-13 21:41:14 -0300

    envio veiculo

--- TrabalhoFinalbee/TesteOrientacoesObjeto/Program.cs
+++ TrabalhoFinalbee/TesteOrientacoesObjeto/Program.cs
@@ -6,7 +6,45 @@ namespace TesteOrientacoesObjeto
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Escreva uma classe que representa um veículo conforme os requisitos abaixo:
+
+            // 1 A classe deve possuir uma propriedade pública do tipo inteiro para
+            //    armazenar a valocidade atual:
+            // 2 A classe deve possuir um atributo privado do tipo booleano para
+            //    armazenar se o veículo está ligado;
+            // 3 A classe deve possuir um método privado para frear o carro
+            //    (zerar a velocidade atual);
+            // 4 A classe deve possuir um método público para ligar o carro
+            //    (atribuir verdadeiro ao atributo do item 2);
+            // 5 A classe deve possuir um método público para desligar o carro
+            //    (atribuir falso ao atributo do item 2. Se o veículo estiver com 
+            //    uma velocidade atual superior a zero, deve primeiramente frear o mesmo);
+            // 6 A classe deve possuir um método público que retorne se o veículo está
+            //    ligado (atributo do item 2);
+            // 7 A classe deve possuir um método público para acelerar o veículo
+            //     (o método deve receber por parâmetro a nova velocidade e somente
+            //     atribuir ela como velocidade atual se o veículo estiver ligado
+            //     a velocidade for menor ou igual a 100).
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
--- TrabalhoFinalbee/TesteOrientacoesObjeto/veiculo.cs
+++ TrabalhoFinalbee/TesteOrientacoesObjeto/veiculo.cs
@@ -1,12 +1,46 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace TesteOrientacoesObjeto
 {
-    internal class Class1
+    internal class veiculo
     {
+        public int VelocidadeAtual { get; set; }
+
+        private bool VeiculoEstaLigado;
+
+        private void FrearCarro()
+        {
+            VelocidadeAtual = 0;
+        }
+
+        public void LigarCarro()
+        {       
+            VeiculoEstaLigado = true;
+        }
+        public void DesligarCarro()
+        {
+
+            if (VelocidadeAtual == 0)
+            {
+                FrearCarro();
+                VeiculoEstaLigado = false;
+            }
+        }
+        public bool TesteLigado()
+        {
+            return VeiculoEstaLigado;
+
+        }
+
+        public int AcelerarVeiculo(int acelerar)
+        {
+            if (VelocidadeAtual <= 100)
+            {
+                VelocidadeAtual = acelerar;
+            }
+            return VelocidadeAtual;
+        }
     }
+
 }
