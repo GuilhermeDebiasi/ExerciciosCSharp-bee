fe39ce1d4aae5e84fe11b1a034f344637add004c
Guest 03 <guest03@alunos.sc.senac.br>
2022-06-13 21:32:23 -0300

    envio veiculo

---
+++ TrabalhoFinalbee/Encapsulamento/ContaBancaria.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Encapsulamento.Entidades
+{
+    internal class ContaBancaria
+    {
+        #region
+        private Entidades.ContaBancaria _contaBancaria;
+
+        #endregion Atributos
+
+        #region Construtores
+       
+        public ContaBancaria(Entidades.ContaBancaria contaBancaria)
+        {
+            _contaBancaria = contaBancaria;
+        }
+
+        #endregion Construtores
+
+        #region Metodos Publicos
+
+        public void Depositar(decimal valor)
+
+            public decimal ObterSaldoAtual()
+        {
+            return _contaBancaria.Saldo();
+        }
+        public void Sacar(decimal valor);
+
+
+
+
+
+
+
+    }
+    #endregion Metodos Publicos
+}
